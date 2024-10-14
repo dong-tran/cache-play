433e556269439f6b910de80e5bd789af71ac11c7
Dong TRAN <dong.tt@ebisol.vn>
2024-10-14 10:16:35 +0700

    Initial Commit

--- nuxt/app.vue
+++ nuxt/app.vue
@@ -2,8 +2,15 @@
   <NuxtLayout>
     <MyHeader />
     <div class="min-h-96">
+      <div class="absolute inset-0 opacity-30 bg-noise"></div>
       <NuxtPage />
     </div>
     <MyFooter />
   </NuxtLayout>
 </template>
+
+<style>
+.bg-noise {
+  background-image: url('/bg.png');
+}
+</style>
--- nuxt/components/MyFooter.vue
+++ nuxt/components/MyFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-24 flex items-center justify-center">
+  <div class="h-24 flex items-center justify-center transition-opacity ease-out duration-1000 opacity-100">
     <img src="/caching.webp">
   </div>
 </template>
